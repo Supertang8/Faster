f72ce9424c7a6c37fd7b99c0943adfaca594201d
Matthew Borres <matt931998gmail.com>
2018-04-05 20:34:57 +0800

    4 by 4 grid

--- Faster/Assets/PlayerController.cs
+++ Faster/Assets/PlayerController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public GameObject player;
+
+	void FixedUpdate () {
+		if (Input.GetKey == 'a') {
+			Add.Force
+		}
 	}
 }
