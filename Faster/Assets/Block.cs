9499d58f185ba5898655e16648518acf12b01dca
Supertang8 <38047420+Supertang8@users.noreply.github.com>
2018-04-05 15:17:34 +0200

    Row Script

--- Faster/Assets/Block.cs
+++ Faster/Assets/Block.cs
@@ -7,9 +7,12 @@ public class Block : MonoBehaviour {
 	public int number;
 	public bool IsBlack;
 
-	// Use this for initialization
 	void Start () {
+		if (IsBlack == true) {
 		
+			GetComponentInParent<Row>().block(number);
+
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Faster/Assets/Row.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour {
+
+	public int blackBlock;
+
+	public void block (int number) {
+		blackBlock = number;
+	}
+}
