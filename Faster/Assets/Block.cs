00f337d0b1f64103b28d2a57f9a22939b18d7025
Supertang8 <38047420+Supertang8@users.noreply.github.com>
2018-04-05 14:40:27 +0200

    Organised

---
+++ Faster/Assets/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	public int number;
+	public bool IsBlack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
