81b826cab42132bc927534e74e52b1220b50df9a
Matthew Borres <matt931998gmail.com>
2018-04-06 22:35:08 +0800

    Working time (Backward count)

---
+++ Faster/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	
+	private float timeLocal;
+	public GameObject timer;
+
+	void Start(){
+
+		timer = GameObject.FindWithTag("Timer");
+	}
+
+	void Update(){
+		timeLocal = timer.GetComponent<Timer> ().timeCount2;
+		if (timeLocal < 0){
+			Application.Quit ();
+		}
+	}
+}
---
+++ Faster/Assets/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer: MonoBehaviour {
+
+	public Text timeText;
+	public float timeCount;
+	public float timeCount2;
+
+	void Update () {
+
+		float timeLocal = timeCount - Time.time;
+		timeText.text = timeLocal.ToString("00");
+		timeCount2 = timeLocal;
+	}
+}
