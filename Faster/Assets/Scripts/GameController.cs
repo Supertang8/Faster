c915a4422eb78242ea1fac93af93b6b4c133478f
Supertang8 <38047420+Supertang8@users.noreply.github.com>
2018-04-20 15:07:53 +0200

    Streak system

--- Faster/Assets/Scripts/GameController.cs
+++ Faster/Assets/Scripts/GameController.cs
@@ -12,10 +12,17 @@ public class GameController : MonoBehaviour {
 	public Text scoreCount;
 	public float scoreText;
 	public bool alive = true;
+	public float streakLossTime;
+	public float ThreeStreakBonus = 3;
+	public int timePenalty = 5;
+	public GameObject bonus;
 
 	private Timer timer;
+	private float streaktime = 0;
+	private int streak= 0;
 
-	void Start(){
+	void Start()
+	{
 	
 		timer = GameObject.FindWithTag ("Timer").GetComponent<Timer> ();
 		scoreText = 0;
@@ -23,6 +30,16 @@ public class GameController : MonoBehaviour {
 
 
 	void Update () {
+
+		streaktime = streaktime + Time.deltaTime;
+
+		if (streaktime >= streakLossTime) 
+		{
+			streak = 0;
+			Debug.Log ("Streak = 0 - Streak lost");
+			bonus.SetActive (false);
+		}
+
 		if (alive) 
 		{
 			if (Input.GetButtonDown ("First") && nextBlack == 1) {
@@ -61,12 +78,26 @@ public class GameController : MonoBehaviour {
 	void scoreInc(){
 
 		scoreText = scoreText + 3;
+		streaktime = 0.0f;
+		streak = streak + 1;
+		Debug.Log ("streak = " + streak);
+
+		if (streak == 3) {
+			timer.timeCount = timer.timeCount + ThreeStreakBonus;
+			Debug.Log ("+3sec");
+			bonus.SetActive (true);
+		} else {
+			bonus.SetActive (false);
+		}
 
 	}
 
 	void timeDec(){
 	
-		timer.timeCount = timer.timeCount - 5;
+		bonus.SetActive (false);
+		timer.timeCount = timer.timeCount - timePenalty;
+		streak = 0;
+		Debug.Log ("Streak = 0 - Streak lost");
 
 	}
 }
